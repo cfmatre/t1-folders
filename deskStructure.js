38c144634e992cce7f5c653127b59c2b259149cc
Carl Filip Matre <cfmatre@gmail.com>
2018-12-11 16:02:23 +0100

    better

--- deskStructure.js
+++ deskStructure.js
@@ -1,13 +1,13 @@
 import S from '@sanity/desk-tool/structure-builder';
 import sanityClient from '@sanity/client';
 
-const client = sanityClient({
-    projectId: '480iybm6',
-    dataset: 'dev',
-    useCdn: false
-});
+const client = sanityClient({ useCdn: false, dataset: 'dev', projectId: '----' });
 
-const getChildren = async (id) => {
+const childBuilder = async (id) => {
+    // If this is a product I wish to show the editor in the next pane.
+    // If it's a folder, show sub folders.
+    // To determine this I am doing an extra api call for folders with this _id.
+    // (only reason I as for sub folders at the end is to return null if this is the end)
     const folders = await client.fetch(`*[_type == "folder" && _id == "${id}"] {foldername} + *[_type == "folder" && parent._ref == "${id}"] {foldername}`);
     if (folders.length === 0) {
         return S.editor()
@@ -23,7 +23,7 @@ const getChildren = async (id) => {
             .title(folders[0].foldername)
             .filter('_type in ["folder", "product"] && ($id == parent._ref || $id == inFolder._ref)')
             .params({ id })
-            .child(getChildren)
+            .child(childBuilder)
     }
 }
 
@@ -31,14 +31,9 @@ export default async () =>
     S.list()
         .title('Content')
         .items([
-            S.listItem()
-                .title('Folder structure')
-                .child(
-                    S.documentList()
-                        .title('Root folders')
-                        .menuItems(S.documentTypeList('folder').getMenuItems())
-                        .filter('_type == "folder" && !defined(parent)')
-                        .child(getChildren)
+            S.listItem().title('Folder structure').child(
+                    S.documentList().title('Root folders').menuItems(S.documentTypeList('folder').getMenuItems())
+                        .filter('_type == "folder" && !defined(parent)').child(childBuilder)
                 ),
             ...S.documentTypeListItems().filter(listItem => !['config'].includes(listItem.getId()))
         ]);
