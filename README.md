a83cd63efe7e3e332816260b6e1d2b71108b0332
Carl Filip Matre <cfmatre@gmail.com>
2020-09-10 11:47:07 +0200

    upgrade for security warning

--- README.md
+++ README.md
@@ -1,7 +1,10 @@
 # Testing folders
 
+Folders and subfolders.
+
+Also testing running migrations in desk.. ,'-/
+
 If you want to make your own data project you can do `sanity install`, I think. Or you can connect it to a project (https://manage.sanity.io/). And put your projectId in the _api.projectId_ part of _sanity.json_.
 
 Also you can import my dataset to your solution with  
 `> sanity dataset import all.tar.gz dev`
-
--- deskStructure.js
+++ deskStructure.js
@@ -16,22 +16,13 @@ const childBuilder = async (id) => {
     // (only reason I as for sub folders at the end is to return null if this is the end)
     const folders = await client.fetch(`*[_type == "folder" && _id == "${id}"] {foldername} + *[_type == "folder" && parent._ref == "${id}"] {foldername}`);
     if (folders.length === 0) {
-        return S.editor()
-            .id('product')
-            .schemaType("product")
-            .documentId(id)
-    }
-    else if (folders.length === 1) {
+        return S.editor().id('product').schemaType('product').documentId(id);
+    } else if (folders.length === 1) {
         return null;
+    } else {
+        return S.documentList().title(folders[0].foldername).filter('_type in ["folder", "product"] && ($id == parent._ref || $id == inFolder._ref)').params({ id }).child(childBuilder);
     }
-    else {
-        return S.documentList()
-            .title(folders[0].foldername)
-            .filter('_type in ["folder", "product"] && ($id == parent._ref || $id == inFolder._ref)')
-            .params({ id })
-            .child(childBuilder)
-    }
-}
+};
 
 export default async () =>
     S.list()
@@ -40,83 +31,78 @@ export default async () =>
             S.listItem()
                 .title('Folder structure')
                 .child(
-                    S.documentList()
+                    S.documentList() //
                         .title('Root folders')
                         .menuItems(S.documentTypeList('folder').getMenuItems())
                         .filter('_type == "folder" && !defined(parent)')
                         .child(childBuilder)
                 ),
-            S.listItem()
-                .title('Ordnung mus zein')
-                .child(
-                    S.editor()
-                        .id('myFolderOrdering')
-                        .schemaType('myFolderOrdering')
-                        .documentId('my-folder-ordering')
-                ),
-            ...S.documentTypeListItems()
-                .filter(listItem => !['config'].includes(listItem.getId())),
+            S.listItem().title('Ordnung mus zein').child(
+                S.editor() //
+                    .id('myFolderOrdering')
+                    .schemaType('myFolderOrdering')
+                    .documentId('my-folder-ordering')
+            ),
+            ...S.documentTypeListItems().filter((listItem) => !['config'].includes(listItem.getId())),
             extraF.testMigrate(),
-
         ]);
 
 const extraF = {
-    testMigrate: _ => {
+    testMigrate: (_) => {
         return S.listItem()
             .title('Test Migrate')
             .child(() => {
                 // console.log('(:(|)');
                 extraF.getKey();
                 return null;
-            })
+            });
     },
-    getKey: _ => {
+    getKey: (_) => {
         console.log('getKey');
         const groq = '*[_type == $type && defined(description) && !defined(description2)] {_id, title, description, description2}';
         const params = {
-            "type": "category",
-            "field1": "description",
-            "field2": "description2",
-        }
-        client.fetch(groq, params).then(resultSet => {
+            type: 'category',
+            field1: 'description',
+            field2: 'description2',
+        };
+        client.fetch(groq, params).then((resultSet) => {
             console.log(resultSet);
 
             for (let e of resultSet) {
                 if (e.title == '_test') {
-
                     console.log(e);
                     const d1Array = e.description.trim().split(/\r\n?|\n\r?/);
-                    const d2Array = d1Array.map(b => {
+                    const d2Array = d1Array.map((b) => {
                         return {
                             _type: 'block',
                             children: [
                                 {
                                     _type: 'span',
                                     // marks: [],
-                                    text: b.trim()
-                                }
+                                    text: b.trim(),
+                                },
                             ],
                             level: 1,
                             listItem: 'bullet',
                             markDefs: [],
-                            style: 'normal'
-                        }
+                            style: 'normal',
+                        };
                     });
 
-                    client.patch(e._id)
+                    client
+                        .patch(e._id)
                         .setIfMissing({ description2: d2Array })
                         .commit()
-                        .then(n => {
-                            console.log('Hurray, updated!')
-                            console.log(n)
+                        .then((n) => {
+                            console.log('Hurray, updated!');
+                            console.log(n);
                         })
-                        .catch(err => {
-                            console.error('Oh no, the update failed: ', err.message)
-                        })
+                        .catch((err) => {
+                            console.error('Oh no, the update failed: ', err.message);
+                        });
                 }
-
             }
-        })
+        });
         // const d2 = [
         //     {
         //         "_type": "block",
@@ -148,9 +134,7 @@ const extraF = {
         //     }
         // ]
     },
-    testOk: _ => {
-        return S.listItem()
-            .title('TEST OK')
-            .child()
+    testOk: (_) => {
+        return S.listItem().title('TEST OK').child();
     },
-}
+};
